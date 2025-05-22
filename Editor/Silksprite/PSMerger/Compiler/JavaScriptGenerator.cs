17aafe5fa782b41c9e500988f8c962a6565d80ba
kaikoga <info@mail.kaikoga.net>
2025-05-23 01:56:56 +0900

    codegen PlayerScript Preamble

--- Editor/Silksprite/PSMerger/Compiler/JavaScriptGenerator.cs
+++ Editor/Silksprite/PSMerger/Compiler/JavaScriptGenerator.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text;
 using ClusterVR.CreatorKit.Item.Implements;
 
 namespace Silksprite.PSMerger.Compiler
@@ -40,64 +42,91 @@ const $$ = (() => {
 })();
 ";
 
-        const string PlayerScriptPreamble = @"
-const __ = (() => {
-    const h = new Proxy(new Map(), {
-        get: function(target, prop) { return target.has(prop) ? target.get(prop) : target.set(prop, new Map()).get(prop); }
-    });
-    _.onFrame((...args) => {
-        for (const f of h.onFrame.values()) f(...args);
-    });
-    _.onReceive((...args) => {
-        for (const f of h.onReceive.values()) f(...args);
-    });
-    _.oscHandle.onReceive((...args) => {
-        for (const f of h.onOscReceive.values()) f(...args);
-    });
-    function createProxy(obj, base) {
-        return new Proxy(obj, {
-            get(target, prop, receiver) {
-                let thisValue = target; 
-                let value = target[prop];
-                if (value === undefined) {
-                    thisValue = base;
-                    value = base[prop];
-                }
-                return (value instanceof Function) ? value.bind(thisValue) : value;
-            }
-        });
-    }
-    return () => {
-        const _oscHandle = createProxy({
-            onOscReceive(callback) { h.onOscReceive.set(this, callback) },
-        }, _.oscHandle);
-        return createProxy({
-            onFrame(callback) { h.onFrame.set(this, callback) },
-            onReceive(callback) { h.onReceive.set(this, callback) },
-            get oscHandle() { return _oscHandle },
-        }, _);
-    }
-})();
-";
-
+        
+        readonly string _g;
+        readonly string _gg;
         readonly string _preamble;
-        readonly string _globalHandle;
-        readonly string _globalContextProvider;
 
         public JavaScriptGenerator(bool playerScript)
         {
-            _preamble = playerScript ? PlayerScriptPreamble : ItemScriptPreamble;
-            _globalHandle = playerScript ? "_" : "$";
-            _globalContextProvider = playerScript ? "__" : "$$";
+            _g = playerScript ? "_" : "$";
+            _gg = playerScript ? "__" : "$$";
+            _preamble = playerScript 
+                ? BuildPreamble(new (string, string, string)[]
+                {
+                    (null, "onFrame", null),
+                    (null, "onReceive", null),
+                    ("oscHandle", "onOscReceive", null)
+                })
+                : ItemScriptPreamble;
         }
 
         public string MergeScripts(JavaScriptAsset[][] scripts)
         {
             return _preamble + string.Join("\n", scripts.Select(context => $@"
-({_globalHandle} => {{
+({_g} => {{
 {string.Join("\n", context.Select(ps => ps != null ? ps.text : null))}
-}})({_globalContextProvider}());
+}})({_gg}());
 "));
+        }
+
+        [SuppressMessage("ReSharper", "RedundantStringInterpolation")]
+        string BuildPreamble((string obj, string name, string args)[] callbackDefs)
+        {
+            var callbacks = callbackDefs
+                .Select(def => (def.obj, def.name, path: def.obj is null ? def.name : $"{def.obj}.{def.name}", def.args))
+                .ToArray();
+            var objs = callbacks
+                .Where(c => c.obj is not null)
+                .GroupBy(c => c.obj)
+                .ToArray();
+            var sb = new StringBuilder();
+            
+            sb.AppendLine($"const {_gg} = (() => {{");
+            sb.AppendLine($"  const h = new Proxy(new Map(), {{");
+            sb.AppendLine($"    get: function(target, prop) {{ return target.has(prop) ? target.get(prop) : target.set(prop, new Map()).get(prop); }}");
+            sb.AppendLine($"  }});");
+            foreach (var callback in callbacks)
+            {
+                sb.AppendLine($"  {_g}.{callback.path}((...args) => {{");
+                sb.AppendLine($"    for (const f of h.{callback.name}.values()) f(...args);");
+                sb.AppendLine($"  }});");
+            }
+            sb.AppendLine($"  function createProxy(obj, base) {{");
+            sb.AppendLine($"    return new Proxy(obj, {{");
+            sb.AppendLine($"      get(target, prop, receiver) {{");
+            sb.AppendLine($"        let thisValue = target; ");
+            sb.AppendLine($"        let value = target[prop];");
+            sb.AppendLine($"        if (value === undefined) {{");
+            sb.AppendLine($"          thisValue = base;");
+            sb.AppendLine($"          value = base[prop];");
+            sb.AppendLine($"        }}");
+            sb.AppendLine($"        return (value instanceof Function) ? value.bind(thisValue) : value;");
+            sb.AppendLine($"      }}");
+            sb.AppendLine($"    }});");
+            sb.AppendLine($"  }}");
+            sb.AppendLine($"  return () => {{");
+            foreach (var obj in objs)
+            {
+                sb.AppendLine($"    const _{obj.Key} = createProxy({{");
+                foreach (var c in obj)
+                {
+                    sb.AppendLine($"      {c.name}(callback) {{ h.{c.name}.set(this, callback) }},");
+                }
+                sb.AppendLine($"    }}, {_g}.{obj.Key});");
+            }
+            sb.AppendLine($"    return createProxy({{");
+            sb.AppendLine($"      onFrame(callback) {{ h.onFrame.set(this, callback) }},");
+            sb.AppendLine($"      onReceive(callback) {{ h.onReceive.set(this, callback) }},");
+            foreach (var obj in objs.Select(o => o.Key))
+            {
+                sb.AppendLine($"      get {obj}() {{ return _{obj} }},");
+            }
+            sb.AppendLine($"    }}, {_g});");
+            sb.AppendLine($"  }}");
+            sb.AppendLine($"}})();");
+
+            return sb.ToString();
         }
     }
 }
